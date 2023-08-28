c91534f8828a58248ee997117b8b31212e309cf0
burhanuddin limdi <burhanuddinlimdiwala5123@gmail.com>
2023-08-28 19:13:08 +0530

    music player updated

--- lib/widgets/music_player.dart
+++ lib/widgets/music_player.dart
@@ -3,8 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:limusic/API/api.dart';
 import 'package:limusic/blocs/root_bloc/root_bloc.dart';
 import 'package:limusic/services/audio_manager.dart';
+import 'package:limusic/services/download_manager.dart';
 import 'package:limusic/utilities/formatter.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
@@ -42,9 +44,32 @@ class MusicPlayer extends StatefulWidget {
 }
 
 class _MusicPlayerState extends State<MusicPlayer> {
+  late bool songLiked;
+  @override
+  void initState() {
+    super.initState();
+    songLiked = checkForSongLiked(widget.song.id.toString());
+  }
+
   @override
   Widget build(context) {
-    return SizedBox(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: RadialGradient(
+          colors: [
+            Theme.of(context).colorScheme.secondary,
+            Theme.of(context).colorScheme.primary,
+          ], // Replace with your desired colors
+          center: Alignment.center,
+          radius: 0.6,
+          focal: Alignment.center,
+          focalRadius: 0.1,
+        ),
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
       height: MediaQuery.of(context).size.height - 25,
       width: double.infinity,
       child: StreamBuilder<PositionData>(
@@ -65,9 +90,29 @@ class _MusicPlayerState extends State<MusicPlayer> {
                 children: [
                   Container(
                     width: 250,
-                    padding: const EdgeInsets.only(top: 50),
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Text(
+                      'Now Playing',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width - 50),
+                    padding: const EdgeInsets.only(top: 30),
                     child: Text(
                       formatSongTitle(widget.song.title.toString()),
+                      overflow: TextOverflow.ellipsis,
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         color: Theme.of(context).colorScheme.secondary,
@@ -109,15 +154,14 @@ class _MusicPlayerState extends State<MusicPlayer> {
                         initialValue:
                             positionData.position.inMilliseconds.toDouble(),
                         appearance: CircularSliderAppearance(
-                          size: 300,
+                          size: 350,
                           startAngle: -60,
                           angleRange: 300,
                           customColors: CustomSliderColors(
-                            trackColor: Theme.of(context).colorScheme.secondary,
+                            trackColor: Theme.of(context).colorScheme.primary,
                             progressBarColors: [
                               const Color(0xffffae00),
                               Theme.of(context).scaffoldBackgroundColor,
-                              Theme.of(context).colorScheme.primary,
                             ],
                           ),
                           customWidths: CustomSliderWidths(
@@ -138,8 +182,8 @@ class _MusicPlayerState extends State<MusicPlayer> {
                     Padding(
                       padding: const EdgeInsets.only(top: 10),
                       child: Container(
-                        width: 260,
-                        height: 260,
+                        width: 320,
+                        height: 320,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(999),
                           border: Border.all(color: Colors.black, width: 0.5),
@@ -181,114 +225,159 @@ class _MusicPlayerState extends State<MusicPlayer> {
                   final playing = playerState.playing;
 
                   if (processingState == ProcessingState.loading ||
-                      processingState == ProcessingState.buffering) {
-                    return const CircularProgressIndicator(
-                      color: Colors.white,
+                      processingState == ProcessingState.buffering ||
+                      processingState == ProcessingState.idle) {
+                    return const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircularProgressIndicator(
+                        color: Colors.white,
+                      ),
                     );
                   }
+
+                  void playNextSong() async {
+                    audioPlayer.seek(const Duration(milliseconds: 0));
+                    audioPlayer.stop();
+                    final newSong = await playNext(
+                      song: widget.song,
+                      playlist: widget.playlist,
+                    );
+                    if (newSong != null) {
+                      setState(() {
+                        widget.song = newSong;
+                      });
+                      widget.setSongState(newSong);
+                    }
+                  }
+
+                  audioPlayer.processingStateStream.listen((state) {
+                    if (state == ProcessingState.completed) {
+                      print('Song has finished playing!');
+                      playNextSong();
+                    }
+                  });
 
                   final currnetPosition = positionData.position.inMilliseconds;
                   final totalDuration = positionData.duration.inMilliseconds;
 
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        onPressed: () async {
-                          audioPlayer.stop();
-                          audioPlayer.seek(const Duration(milliseconds: 0));
-                          final newSong = await playPrevious(
-                            song: widget.song,
-                            playlist: widget.playlist,
-                          );
-                          if (newSong != null) {
-                            setState(() {
-                              widget.song = newSong;
-                            });
-                            widget.setSongState(newSong);
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.skip_previous_rounded,
-                          color: Colors.white,
-                          size: 35,
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: () => downloadSong(widget.song),
+                          icon: const Icon(
+                            Icons.download_rounded,
+                            color: Colors.white,
+                            size: 25,
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          if (currnetPosition > 10000) {
-                            audioPlayer.seek(
-                              Duration(
-                                milliseconds: currnetPosition.toInt() - 10000,
-                              ),
+                        IconButton(
+                          onPressed: () async {
+                            audioPlayer.stop();
+                            audioPlayer.seek(const Duration(milliseconds: 0));
+                            final newSong = await playPrevious(
+                              song: widget.song,
+                              playlist: widget.playlist,
                             );
-                          } else {
-                            audioPlayer.seek(Duration.zero);
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.replay_10_rounded,
-                          color: Colors.white,
-                          size: 35,
+                            if (newSong != null) {
+                              setState(() {
+                                widget.song = newSong;
+                              });
+                              widget.setSongState(newSong);
+                            }
+                          },
+                          icon: const Icon(
+                            Icons.skip_previous_rounded,
+                            color: Colors.white,
+                            size: 25,
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed:
-                            playing ? audioPlayer.pause : audioPlayer.play,
-                        icon: Icon(
-                          playing ? Icons.pause : Icons.play_arrow,
-                          color: Colors.white,
-                          size: 60,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          if (currnetPosition != totalDuration) {
-                            if (totalDuration.toInt() -
-                                    currnetPosition.toInt() >
-                                10000) {
+                        IconButton(
+                          onPressed: () {
+                            if (currnetPosition > 10000) {
                               audioPlayer.seek(
                                 Duration(
-                                  milliseconds: currnetPosition.toInt() + 10000,
+                                  milliseconds: currnetPosition.toInt() - 10000,
                                 ),
                               );
                             } else {
-                              audioPlayer.seek(
-                                Duration(
-                                  milliseconds: totalDuration.toInt(),
-                                ),
-                              );
+                              audioPlayer.seek(Duration.zero);
                             }
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.forward_10,
-                          color: Colors.white,
-                          size: 35,
+                          },
+                          icon: const Icon(
+                            Icons.replay_10_rounded,
+                            color: Colors.white,
+                            size: 25,
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () async {
-                          audioPlayer.stop();
-                          audioPlayer.seek(const Duration(milliseconds: 0));
-                          final newSong = await playNext(
-                            song: widget.song,
-                            playlist: widget.playlist,
-                          );
-                          if (newSong != null) {
+                        IconButton(
+                          onPressed:
+                              playing ? audioPlayer.pause : audioPlayer.play,
+                          icon: Icon(
+                            playing ? Icons.pause : Icons.play_arrow,
+                            color: Colors.white,
+                            size: 60,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            if (currnetPosition != totalDuration) {
+                              if (totalDuration.toInt() -
+                                      currnetPosition.toInt() >
+                                  10000) {
+                                audioPlayer.seek(
+                                  Duration(
+                                    milliseconds:
+                                        currnetPosition.toInt() + 10000,
+                                  ),
+                                );
+                              } else {
+                                audioPlayer.seek(
+                                  Duration(
+                                    milliseconds: totalDuration.toInt(),
+                                  ),
+                                );
+                              }
+                            }
+                          },
+                          icon: const Icon(
+                            Icons.forward_10,
+                            color: Colors.white,
+                            size: 25,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            playNextSong();
+                          },
+                          icon: const Icon(
+                            Icons.skip_next_rounded,
+                            color: Colors.white,
+                            size: 25,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            String sondId = widget.song.id.toString();
+                            songLiked
+                                ? onDislikeSong(sondId)
+                                : onLikeSong(sondId);
                             setState(() {
-                              widget.song = newSong;
+                              songLiked = checkForSongLiked(sondId);
                             });
-                            widget.setSongState(newSong);
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.skip_next_rounded,
-                          color: Colors.white,
-                          size: 35,
+                          },
+                          icon: Icon(
+                            songLiked
+                                ? Icons.favorite
+                                : Icons.favorite_border_rounded,
+                            color: Colors.white,
+                            size: 25,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   );
                 },
               ),
