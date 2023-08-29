103b33379422159c3941457a62b50e65ab15c148
burhanuddin limdi <burhanuddinlimdiwala5123@gmail.com>
2023-08-29 12:53:21 +0530

    download song fixed

--- lib/services/download_manager.dart
+++ lib/services/download_manager.dart
@@ -5,14 +5,15 @@ import 'package:limusic/services/data_manager.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> downloadSong(dynamic song) async {
-  // print('download pressed');
+  print('download pressed');
   final songUrl = await getSong(song.id.toString(), song.isLive);
-  var status = await Permission.storage.request();
+  var status = await Permission.manageExternalStorage.request();
   String fileName = song.title.toString();
+  print(status);
   if (status.isPermanentlyDenied) {
     openAppSettings();
   } else if (status.isDenied) {
-    Permission.storage.request();
+    Permission.manageExternalStorage.request();
   } else if (status.isGranted) {
     final directory = Directory('/storage/emulated/0/Download');
 
@@ -35,5 +36,11 @@ Future<void> downloadSong(dynamic song) async {
 Future<void> checkNecessaryPermissions() async {
   await Permission.audio.request();
   await Permission.notification.request();
-  await Permission.storage.request();
+  // await Permission.storage.request();
+  // await Permission.manageExternalStorage.request();
+  try {
+    await Permission.storage.request();
+  } catch (e) {
+    print(e);
+  }
 }
