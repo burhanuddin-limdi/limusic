0163625fcf31afb5fad0c1f814e2b8dc65623a73
burhanuddin limdi <burhanuddinlimdiwala5123@gmail.com>
2023-08-29 15:02:35 +0530

    download problem fixed

--- lib/services/download_manager.dart
+++ lib/services/download_manager.dart
@@ -1,46 +1,65 @@
 import 'dart:io';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:limusic/API/api.dart';
 import 'package:limusic/services/data_manager.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 Future<void> downloadSong(dynamic song) async {
-  print('download pressed');
-  final songUrl = await getSong(song.id.toString(), song.isLive);
-  var status = await Permission.manageExternalStorage.request();
-  String fileName = song.title.toString();
-  print(status);
-  if (status.isPermanentlyDenied) {
-    openAppSettings();
-  } else if (status.isDenied) {
-    Permission.manageExternalStorage.request();
-  } else if (status.isGranted) {
-    final directory = Directory('/storage/emulated/0/Download');
+  if (Platform.isAndroid) {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    final androidVersion = int.parse(androidInfo.version.release);
 
-    if (!await directory.exists()) {
-      await directory.create(recursive: true);
+    final songUrl = await getSong(song.id.toString(), song.isLive);
+
+    final status;
+
+    if (androidVersion < 13) {
+      status = await Permission.storage.request();
+    } else {
+      status = await Permission.manageExternalStorage.request();
     }
+    // dynamic status = int.parse(androidInfo.version.release) < 13
+    //     ? await Permission.storage.request()
+    //     : Permission.manageExternalStorage.request();
 
-    await FlutterDownloader.enqueue(
-      url: songUrl,
-      fileName: '$fileName.mp3',
-      headers: {},
-      savedDir: directory.path,
-      showNotification: true,
-      openFileFromNotification: true,
-    );
-    addOrUpdateData('user', 'downloadedSongName', fileName);
+    String fileName = song.title
+        .toString()
+        .replaceAll(
+          RegExp(r'[^\w\s-]'),
+          '',
+        )
+        .replaceAll(RegExp(r'(\s)+'), '-');
+    if (status == PermissionStatus.permanentlyDenied) {
+      openAppSettings();
+    } else if (status == PermissionStatus.denied) {
+      Permission.storage.request();
+    } else if (status == PermissionStatus.restricted) {
+      openAppSettings();
+    } else if (status == PermissionStatus.granted) {
+      final directory = Directory('/storage/emulated/0/Download');
+
+      if (!await directory.exists()) {
+        await directory.create(recursive: true);
+      }
+      await FlutterDownloader.enqueue(
+        url: songUrl,
+        fileName: '$fileName.mp3',
+        headers: {},
+        savedDir: directory.path,
+        showNotification: true,
+        openFileFromNotification: true,
+      );
+
+      addOrUpdateData('user', 'downloadedSongName', fileName);
+    }
   }
 }
 
 Future<void> checkNecessaryPermissions() async {
   await Permission.audio.request();
   await Permission.notification.request();
-  // await Permission.storage.request();
-  // await Permission.manageExternalStorage.request();
-  try {
-    await Permission.storage.request();
-  } catch (e) {
-    print(e);
-  }
+  await Permission.storage.request();
+  await Permission.manageExternalStorage.request();
 }
